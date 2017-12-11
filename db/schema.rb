fbb5418aeacd9f857c9fe922ddae31150acb0cae
vagrant <lovemore_jokonya@yahoo.com>
2017-12-11 20:37:40 +0000

    rolled back tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211201211) do
+ActiveRecord::Schema.define(version: 20171211111110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,11 +74,6 @@ ActiveRecord::Schema.define(version: 20171211201211) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-  end
-
-  create_table "widgets", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "line_items", "orders"
